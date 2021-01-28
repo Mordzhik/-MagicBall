8a0f9762bf60b5cf364493015feca6cee1e77a86
Marzhan <nurl-imarzhan@mail.ru>
2021-01-28 23:50:51 +0600

    Initial Commit

---
+++ What do I do/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What do I do
+//
+//  Created by tse on 1/28/21.
+//  Copyright © 2021 marzhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
